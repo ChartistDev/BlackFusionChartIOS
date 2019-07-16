c92b145aa5f26e5e9aac7168e497df05fdbb704f
Debanjan Dey <debanjandey@FCs-MacBook-Pro.local>
2019-07-16 19:00:07 +0530

    added solution2 using deployurl

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,14 +3,21 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { FusionChartsModule } from 'angular-fusioncharts';
+
+// Import FusionCharts library and chart modules
+import * as FusionCharts from 'fusioncharts';
+import * as Charts from 'fusioncharts/fusioncharts.charts';
 
 import { HomePage } from './home.page';
+FusionChartsModule.fcRoot(FusionCharts, Charts);
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    FusionChartsModule,
     RouterModule.forChild([
       {
         path: '',
